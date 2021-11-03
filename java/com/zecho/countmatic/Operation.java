c62c4faf03a68463ad694ac3ddfa2280e28be9bc
Zecho <muhammadzeeshan1670@gmail.com>
2021-11-04 00:32:58 +0500

    v0.1

---
+++ java/com/zecho/countmatic/Operation.java
@@ -0,0 +1,17 @@
+package com.zecho.countmatic;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class Operation {
+
+    public void changeTally(TextView tv, int digit){
+        tv.setText(String.format(Locale.getDefault(),"%04d.", digit));
+    }
+
+    public void changeStep(TextView tv, int value){
+        tv.setText(String.format(Locale.getDefault(),"%02d.", value));
+    }
+}
---
+++ java/com/zecho/countmatic/Splash.java
@@ -0,0 +1,28 @@
+package com.zecho.countmatic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int SPLASH_TIME_OUT = 2000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(Splash.this, MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
